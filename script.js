601f9b187461d7b393763ced396094bc14d58d71
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2021-05-17 15:45:25 +0200

    worked on weather

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const APIURL = 'https://api.github.com/users/';
+// const WAPIURL = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+// d2fe54a679c5bff6646febe2654c5695
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
@@ -12,6 +14,8 @@ async function getUser(username) {
 
     createUserCard(respData);
 
+    getWeather(username);
+
     getRepos(username);
 }
 
@@ -22,24 +26,36 @@ async function getRepos(username) {
     addReposToCard(respData);
 }
 
+async function getWeather(username) {
+    
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+
+    const wea = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + respData.location + '&units=metric&lang=de&appid=d2fe54a679c5bff6646febe2654c5695')
+    const weaData = await wea.json();
+
+    console.log(weaData.name);
+    console.log(weaData.main['temp']);
+}
+
 
 function createUserCard(user) {
     const cardHTML = `
         <div class="card">
-        <div class="year-bg"><b>${user.created_at.slice(0,4)}</b></div>
-            <div>
+        <div class="year-bg"><b>${user.created_at.slice(0, 4)}</b></div>
+            <div class="img-profile">
                 <a href="${user.html_url}" target="_blank"><img class="avatar" src="${user.avatar_url}" alt="${user.name}" /></a>
             </div>
 
             <div class="user-info">
                 <h2>${user.name}<a href="https://twitter.com/${user.twitter_username}" target="_blank"><i class="icon fab fa-twitter"></i></a></h2>
                 <p>${user.bio}</p>
-                <p>${user.location}</p>
+                <p>${user.location}<div id="weather"></div></p>
 
                 <ul class="info">
-                    <li>${user.followers}<strong>Followers</strong></li>
-                    <li>${user.following}<strong>Following</strong></li>
-                    <li>${user.public_repos}<strong>Repos</strong></li>
+                    <li><a href="${user.html_url}?tab=followers" target="_blank">${user.followers}<strong>Followers</strong></a></li>
+                    <li><a href="${user.html_url}?tab=following" target="_blank">${user.following}<strong>Following</strong></a></li>
+                    <li><a href="${user.html_url}?tab=repositories" target="_blank">${user.public_repos}<strong>Repos</strong></a></li>
                 </ul>
 
                 <h4>Repos:</h4>
@@ -68,6 +84,13 @@ function addReposToCard(repos) {
 
             reposEl.appendChild(repoEl);
         })
+}
+
+function createWeather(weather) {
+    const weaEl = document.getElementById("weather");
+
+    weather
+
 }
 
 form.addEventListener("submit", (e) => {
